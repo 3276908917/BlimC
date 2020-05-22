e575fba039f4626dbe92ae6206f5eaa84236ba68
Lukas Finkbeiner @ Puyi <lfinkbeiner@berkeley.edu>
2020-05-21 21:04:31 -0700

    Revert "Fix Double Quote Inconsistencies in the Tests"
    This reverts commit 58dd075860f4090bf9b677b369b4c8d36d571dc5.

--- test_expansions/current_tests/test_h52fil.py
+++ test_expansions/current_tests/test_h52fil.py
@@ -10,8 +10,8 @@ from tests.data import voyager_h5
 
 
 def test_h52fil_conversion():
-    """ Tests the conversion of fil files into h5 in both light and heavy modes.
-    """
+    ''' Tests the conversion of fil files into h5 in both light and heavy modes.
+    '''
 
     # Creating test file.
     bl.h52fil.make_fil_file(voyager_h5, new_filename='test.fil')
@@ -27,24 +27,35 @@ def test_h52fil_conversion():
     os.remove('test_large.fil')
 
 def test_help():
-    """
+    '''
     The user of these tests should verify that the help statement
     was printed as expected; this test merely verifies that the
     system exited.
-    """
+    '''
     with pytest.raises(SystemExit):
         bl.h52fil.cmd_tool(['-h'])
 
+'''
+Okay, congratulations and all; here is your gold medal.
+However: the valuable test is one which checks cmd_tool functionality
+    in a way OTHER than just making sure that it exits!
+    Yeah, I get it, you can catch syntax errors (...right?)
+    But otherwise the cmd_tool is going to issue a SystemExit
+    regardless of your actual input. So your current tests
+    are fairly bad!
+'''
+
 def test_cmd_tool():
-    """
+    '''
     This is the same large test file, but now through the cmd tool.
-    """
+    '''
+    #with pytest.raises(SystemExit):
     bl.h52fil.cmd_tool([voyager_h5, '-n', 'cmd.fil', '-l', '.001'])
 
 def test_no_args():
-    """
+    '''
     The cmd tool needs to exit, mandating a file name.
-    """
+    '''
     with pytest.raises(SystemExit):
         bl.h52fil.cmd_tool()
 
